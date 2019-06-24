c071adf342b72514306916986b9c52c5281902f0
Florian Otto <florian.otto@mindbird.de>
2019-06-24 14:27:30 +0200

    Initial

--- src/ContentElement/Box.php
+++ src/ContentElement/Box.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Mindbird\Contao\CEBox\Controller;
+namespace Mindbird\Contao\CEBox\ContentElement;
 
 use Contao\ContentModel;
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
