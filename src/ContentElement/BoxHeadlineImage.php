4585afd030ad6716a85b6a0a104ff9b17c08c198
Florian Otto <florian.otto@mindbird.de>
2019-07-17 11:57:26 +0200

    WOD

---
+++ src/ContentElement/BoxHeadlineImage.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of [mindbird/contao-ce-box].
+ *
+ * (c) mindbird
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Mindbird\Contao\CEBox\ContentElement;
+
+use Contao\ContentModel;
+use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
+use Contao\FilesModel;
+use Contao\PageModel;
+use Contao\StringUtil;
+use Contao\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class BoxHeadlineImage extends AbstractContentElementController
+{
+    protected function getResponse(Template $template, ContentModel $model, Request $request): Response
+    {
+        $arrHeadline = StringUtil::deserialize($model->headline);
+        $template->headline = \is_array($arrHeadline) ? $arrHeadline['value'] : $arrHeadline;
+        $template->hl = \is_array($arrHeadline) ? $arrHeadline['unit'] : 'h1';
+        $template->text = $model->text;
+        $template->slogan = $model->slogan;
+
+        $file = FilesModel::findByPk($model->singleSRC);
+        Template::addImageToTemplate($template, [
+            'singleSRC' => $file->path,
+            'size' => StringUtil::deserialize($model->imgSize),
+        ]);
+
+        $page = PageModel::findByPk($model->jumpTo);
+        if ($page !== null) {
+            $template->link = $page->getFrontendUrl();
+        } else {
+            $template->link = '';
+        }
+
+        return $template->getResponse();
+    }
+}
