034a3bbdf35ff99dff3199c22839bb8ba0953a34
Florian Otto <florian.otto@mindbird.de>
2019-07-17 12:06:47 +0200

    WOD

---
+++ src/Resources/contao/languages/de/default.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['TL_LANG']['CTE']['boxes'] = 'Boxen';
+$GLOBALS['TL_LANG']['CTE']['box'] = 'Box';
+$GLOBALS['TL_LANG']['CTE']['box_headline_text'] = 'Box (Überschrift, Text)';
+$GLOBALS['TL_LANG']['CTE']['box_headline_image'] = 'Box (Überschrift, Bild)';
--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -2,3 +2,4 @@
 
 $GLOBALS['TL_LANG']['tl_content']['jumpTo'] = ['Seite'];
 $GLOBALS['TL_LANG']['tl_content']['slogan'] = ['Slogan'];
+$GLOBALS['TL_LANG']['tl_content']['box_text'] = ['Text'];
