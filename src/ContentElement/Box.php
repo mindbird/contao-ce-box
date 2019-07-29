34dfb671ed42138ca573ebd2cd269c2354e3acb8
Florian Otto <florian.otto@mindbird.de>
2019-07-29 16:37:06 +0200

    Correct image size from ce settings

--- src/ContentElement/Box.php
+++ src/ContentElement/Box.php
@@ -16,6 +16,7 @@ use Contao\FilesModel;
 use Contao\PageModel;
 use Contao\StringUtil;
 use Contao\Template;
+use function is_array;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -24,16 +25,24 @@ class Box extends AbstractContentElementController
     protected function getResponse(Template $template, ContentModel $model, Request $request): Response
     {
         $arrHeadline = StringUtil::deserialize($model->headline);
-        $template->headline = \is_array($arrHeadline) ? $arrHeadline['value'] : $arrHeadline;
-        $template->hl = \is_array($arrHeadline) ? $arrHeadline['unit'] : 'h1';
+        $template->headline = is_array($arrHeadline) ? $arrHeadline['value'] : $arrHeadline;
+        $template->hl = is_array($arrHeadline) ? $arrHeadline['unit'] : 'h1';
         $template->text = $model->box_text;
         $template->slogan = $model->slogan;
 
         $file = FilesModel::findByPk($model->singleSRC);
-        Template::addImageToTemplate($template, [
-            'singleSRC' => $file->path,
-            'size' => $model->size,
-        ]);
+        if ($file !== null) {
+            Template::addImageToTemplate(
+                $template,
+                [
+                    'singleSRC' => $file->path,
+                    'size' => $model->size,
+                ],
+                null,
+                null,
+                $file
+            );
+        }
 
         $page = PageModel::findByPk($model->jumpTo);
         if ($page !== null) {
