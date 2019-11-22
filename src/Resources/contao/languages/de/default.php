b1e5c5f92067ca107649ce8c204e04717e9e3bd3
Florian Otto <florian.otto@mindbird.de>
2019-11-22 11:23:00 +0100

    Add labels

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -4,3 +4,5 @@ $GLOBALS['TL_LANG']['CTE']['boxes'] = ['Boxen'];
 $GLOBALS['TL_LANG']['CTE']['box'] = ['Box'];
 $GLOBALS['TL_LANG']['CTE']['box_headline_text'] = ['Box (Überschrift, Text)'];
 $GLOBALS['TL_LANG']['CTE']['box_headline_image'] = ['Box (Überschrift, Bild)'];
+$GLOBALS['TL_LANG']['CTE']['box_start'] = ['Box - Start'];
+$GLOBALS['TL_LANG']['CTE']['box_end'] = ['Box  - Ende'];
