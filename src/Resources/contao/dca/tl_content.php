3c634fb2429c5854a6599ab48d63ba2f71504239
Florian Otto <florian.otto@mindbird.de>
2020-02-17 15:50:49 +0100

    Add opportunity to chose from pages files and more as linked element

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -29,6 +29,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['box_text'] = [
     'sql' => "mediumtext NULL"
 ];
 
+$GLOBALS['TL_DCA']['tl_content']['fields']['url']['eval']['mandatory'] = false;
 $GLOBALS['TL_DCA']['tl_content']['fields']['addImage']['eval']['tl_class'] = 'clr';
 
 $GLOBALS['TL_DCA']['tl_content']['palettes']['box'] = '{type_legend},type;{text_legend},headline,box_text,slogan,jumpTo,url,addImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
