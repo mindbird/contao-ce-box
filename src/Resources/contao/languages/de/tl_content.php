c66e71c80d5ad0cc4c38cd5012bc5188a45b506b
Florian Otto <florian.otto@mindbird.de>
2020-02-18 10:36:29 +0100

    Add opportunity to chose from pages files and more as linked element

--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -3,3 +3,4 @@
 $GLOBALS['TL_LANG']['tl_content']['jumpTo'] = ['Seite'];
 $GLOBALS['TL_LANG']['tl_content']['slogan'] = ['Slogan'];
 $GLOBALS['TL_LANG']['tl_content']['box_text'] = ['Text'];
+$GLOBALS['TL_LANG']['tl_content']['link_text'] = ['Link Text'];
