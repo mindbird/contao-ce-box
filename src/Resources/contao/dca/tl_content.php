268c30ee3fdf90e7b9b7901b1ed86db6f71c4bab
Florian Otto <florian.otto@mindbird.de>
2020-02-18 10:15:11 +0100

    Add opportunity to chose from pages files and more as linked element

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,7 +1,6 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['slogan'] = [
-    'label' => &$GLOBALS['TL_LANG']['tl_content']['slogan'],
     'exclude' => true,
     'search' => true,
     'inputType' => 'text',
@@ -10,7 +9,6 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['slogan'] = [
 ];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['jumpTo'] = [
-    'label' => &$GLOBALS['TL_LANG']['tl_content']['jumpTo'],
     'exclude' => true,
     'inputType' => 'pageTree',
     'foreignKey' => 'tl_page.title',
@@ -20,7 +18,6 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['jumpTo'] = [
 ];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['box_text'] = [
-    'label' => &$GLOBALS['TL_LANG']['tl_content']['box_text'],
     'exclude' => true,
     'search' => true,
     'inputType' => 'textarea',
@@ -29,9 +26,17 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['box_text'] = [
     'sql' => "mediumtext NULL"
 ];
 
+$GLOBALS['TL_DCA']['tl_content']['fields']['link_text'] = [
+    'exclude' => true,
+    'search' => true,
+    'inputType' => 'text',
+    'eval' => array('maxlength' => 255, 'tl_class' => 'clr'),
+    'sql' => "varchar(255) NOT NULL default ''"
+];
+
 $GLOBALS['TL_DCA']['tl_content']['fields']['url']['eval']['mandatory'] = false;
 $GLOBALS['TL_DCA']['tl_content']['fields']['addImage']['eval']['tl_class'] = 'clr';
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['box'] = '{type_legend},type;{text_legend},headline,box_text,slogan,jumpTo,url,addImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
-$GLOBALS['TL_DCA']['tl_content']['palettes']['box_headline_image'] = '{type_legend},type;{text_legend},headline,jumpTo,url,addImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
-$GLOBALS['TL_DCA']['tl_content']['palettes']['box_headline_text'] = '{type_legend},type;{text_legend},headline,box_text,jumpTo,url;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['box'] = '{type_legend},type;{text_legend},headline,box_text,slogan,jumpTo,url,link_text,addImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['box_headline_image'] = '{type_legend},type;{text_legend},headline,jumpTo,url,link_text,addImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['box_headline_text'] = '{type_legend},type;{text_legend},headline,box_text,jumpTo,url,link_text;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
